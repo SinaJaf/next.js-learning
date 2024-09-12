7b2a4aacead251a05203d31d658e4f7bb8d2a6f7
Sina Jafari <100541232+SinaJaf@users.noreply.github.com>
2024-09-12 18:16:55 -0400

    Update navbar
    update navigation options for logged in and not logged states

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -10,6 +10,7 @@ import { usePathname } from "next/navigation";
 const NavBar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const pathname = usePathname();
 
   return ( 
@@ -73,16 +74,19 @@ const NavBar = () => {
                   className={`${pathname === '/properties' ? 'bg-black' : ''} text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
                   >Properties
                 </Link>
+                { isLoggedIn && (
                 <Link
                   href="/properties/add"
                   className={`${pathname === '/properties/add' ? 'bg-black' : ''} text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
                   >Add Property
                 </Link>
+                )}
               </div>
             </div>
           </div>
 
           {/* <!-- Right Side Menu (Logged Out) --> */}
+          {!isLoggedIn && (
           <div className="hidden md:block md:ml-6">
             <div class="flex items-center">
               <button
@@ -93,18 +97,19 @@ const NavBar = () => {
               </button>
             </div>
           </div>
+          )}
 
           {/* <!-- Right Side Menu (Logged In) --> */}
+          { isLoggedIn && (
           <div
-            class="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0"
-          >
-            <Link href="/messages.html" class="relative group">
+            className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0">
+            <Link href="/messages.html" className="relative group">
               <button
                 type="button"
-                class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
               >
                 <span className="absolute -inset-1.5"></span>
-                <span class="sr-only">View notifications</span>
+                <span className="sr-only">View notifications</span>
                 <svg
                   className="h-6 w-6"
                   fill="none"
@@ -121,14 +126,14 @@ const NavBar = () => {
                 </svg>
               </button>
               <span
-                class="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full"
+                className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full"
               >
                 2
                 {/* <!-- Replace with the actual number of notifications --> */}
               </span>
             </Link>
             {/* <!-- Profile dropdown button --> */}
-            <div class="relative ml-3">
+            <div className="relative ml-3">
               <div>
                 <button
                   type="button"
@@ -139,7 +144,7 @@ const NavBar = () => {
                   onClick={ () => setIsProfileMenuOpen( (prev) => !prev ) }
                 >
                   <span className="absolute -inset-1.5"></span>
-                  <span class="sr-only">Open user menu</span>
+                  <span className="sr-only">Open user menu</span>
                   <Image
                     className="h-8 w-8 rounded-full"
                     src={ profileDefault }
@@ -187,6 +192,7 @@ const NavBar = () => {
               }
             </div>
           </div>
+          )}
         </div>
       </div>
 
@@ -194,7 +200,7 @@ const NavBar = () => {
       {
         isMobileMenuOpen && (
           <div id="mobile-menu">
-            <div class="space-y-1 px-2 pb-3 pt-2">
+            <div className="space-y-1 px-2 pb-3 pt-2">
               <Link
                 href="/"
                 className={`${pathname === '/' ? 'bg-black' : ''} text-white block rounded-md px-3 py-2 text-base font-medium`}
@@ -205,17 +211,21 @@ const NavBar = () => {
                 className={`${pathname === '/properties' ? 'bg-black' : ''} text-white block rounded-md px-3 py-2 text-base font-medium`}
                 >Properties
               </Link>
+              { isLoggedIn && (
               <Link
                 href="/properties/add"
                 className={`${pathname === '/properties/add' ? 'bg-black' : ''} text-white block rounded-md px-3 py-2 text-base font-medium`}
                 >Add Property
               </Link>
+              )}
+              {!isLoggedIn && (
               <button
                 class="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5"
               >
                 <i className="fa-brands fa-google mr-2"></i>
                 <span>Login or Register</span>
               </button>
+              )}
             </div>
           </div>
         )
