7ef87c88d68b45a62bcf9b425da65358e26bc651
Sina Jafari <100541232+SinaJaf@users.noreply.github.com>
2024-09-16 13:44:21 -0400

    Create prop details
    Created the detail components for property page

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -20,18 +20,40 @@
 //   );
       
 // }
+import PropertyHeaderImage from '@/components/PropertyHeaderImage'
+import Property from '@/models/property';
+import connectDB from '@/config/database';
+import Link from 'next/link';
+import { FaArrowLeft } from 'react-icons/fa';
+import PropertyDetails from '@/components/PropertyDetails';
+import PropertySidebar from '@/components/PropertySideBar';
 
 // Server side component
-const PropertyPage = ({ params, searchParams, pathname }) => {
+const PropertyPage = async ({ params }) => {
+  await connectDB();
+  const property = await Property.findById(params.id).lean()
 
   return (
-    <div className="text-2xl">
-      Properties Page { params.id } <br/> name: { searchParams.name } <br/> path: localhost:3000{ pathname }
-    <br/>
-    <button className='text-[#2563eb]'>
-      Go Home
-    </button> 
-    </div>
+    <>
+      <PropertyHeaderImage image={property.images[0]}/>
+      <section>
+        <div className="container m-auto py-6 px-6">
+          <Link
+            href="/properties"
+            className="text-blue-500 hover:text-blue-600 flex items-center">
+            <FaArrowLeft className='mr-2'/>Back to Properties
+          </Link>
+        </div>
+      </section>
+      <section className="bg-blue-50">
+        <div className="container m-auto py-10 px-6">
+          <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+            <PropertyDetails property={property}/>
+            <PropertySidebar />
+          </div>
+        </div>
+      </section>
+    </>
   );
       
 }
