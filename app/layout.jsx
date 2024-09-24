9d4e4bda5b981d5e66a950c90b2fd4a5b9be9d97
Sina Jafari <100541232+SinaJaf@users.noreply.github.com>
2024-09-24 19:30:57 -0400

    Create the auth API
    Created the authentication for the project using NextAuth and google cloud

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import '@/assets/styles/globals.css'
 import NavBar from '@/components/NavBar'
 import Footer from '@/components/Footer'
+import AuthProvider from '@/components/AuthProvider'
 
 export const metadata = {
   title: 'Property Pulse',
@@ -10,15 +11,17 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
   return ( 
-    <html>
-      <body>
-        <NavBar/>
-        <main>
-          { children }
-        </main>
-        <Footer/>
-      </body>
-    </html> 
+    <AuthProvider>
+      <html>
+        <body>
+          <NavBar/>
+          <main>
+            { children }
+          </main>
+          <Footer/>
+        </body>
+      </html>
+    </AuthProvider>
   );
 }
  
