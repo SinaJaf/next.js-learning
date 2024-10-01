93906bab53de8b48ab3cc24b04c52610c2a55f71
Sina Jafari <100541232+SinaJaf@users.noreply.github.com>
2024-09-30 22:54:28 -0400

    Add SignIn
    updated the authOptions.js file with  the signin function and added it to navbar

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,8 +1,10 @@
-import GoogleProvideer from 'next-auth/provider/google';
+import GoogleProvider from 'next-auth/providers/google';
+import connectDB from '@/config/database';
+import User from '@/models/User';
 
 export const authOptions = {
   providers: [
-    GoogleProvideer({
+    GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       authorization: {
@@ -18,20 +20,33 @@ export const authOptions = {
     // Invoked on successful sign in
     async signIn({ profile }) {
       // 1. Connect to the database
+      await connectDB();
 
       // 2. Check if user exists
+      const userExists = await User.findOne({ email: profile.email });
 
       //3. If not create user
+      if (!userExists) {
+        const username = profile.name.slice(0, 20);
+        await User.create({
+          email: profile.email,
+          username,
+          image: profile.picture,
+        })
+      }
 
       // 4. Return true to allow sign in
+      return true
     },
     // Session callback function that modifies the session object
     async session({ session }) {
       // 1. Get user from database
+      const user = await User.findOne({ email: session.user.email })
 
       // 2. Assign user id from session
-      
-      // 3. Return session
+      session.user.id = user._id.toString();
+
+      return session
     }
   }
 }
